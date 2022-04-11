37caf729f073979f81e2f2c69f749aa62c0e0057
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-11 16:17:11 +0200

    Add SimulatorCard components to SimulatorScreen

--- screens/SimulatorScreen.tsx
+++ screens/SimulatorScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import SimulatorCard from '../components/SimulatorCard';
 
 export default function SimulatorScreen() {
   return (
       <ScrollView style={styles.container}>
+          <SimulatorCard />
       </ScrollView>
   );
 }
