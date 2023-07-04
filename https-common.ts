1497349c4d807ae56c82cb5280f5fcf7444f372f
Ayoub Lahyaoui <ayoub.lahyaoui@hotmail.com>
2023-07-04 22:19:21 +0200

    Update API Base url

--- https-common.ts
+++ https-common.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* Creating a new instance of axios with a base url and headers. */
 export default axios.create({
-    baseURL: "https://curriculum-simulator.herokuapp.com/api",
+    baseURL: "https://curriculum-simulator.onrender.com/api",
     headers: {
         "Content-type": "application/json"
     }
